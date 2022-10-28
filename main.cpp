f53a4107c3c949506cc82b0d50eb6076c2696afd
JimmyP8899 <80071092+JimmyP8899@users.noreply.github.com>
2022-10-27 17:01:20 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 #define aRow 4		//Define A Row
@@ -15,23 +16,41 @@ void print(int output[][outCol]);
 void classicMult(int A[][aCol], int B[][bCol], int output[][outCol]);
 int main() {
 // MAIN METHOD TESTING METHOD
-//	int random = (rand() % 10) + 1;
-//	cout << random;
-//	cout << "\n";
 
+//random number generator for arrays
+/*	srand(time(0));  // Initialize random number generator
+	int A[aRow][aCol];
+	for (int i = 0; i < aRow; i++) {
+		for (int j = 0; j < aCol; j++) {
+			A[i][j] = (rand() % 10);
+		}
+	}
+	int B[bRow][bCol];
+	for (int i = 0; i < bRow; i++) {
+		for (int j = 0; j < bCol; j++) {
+			B[i][j] = (rand() % 10);
+		}
+	}
+	cout << "Arrays successfully generated\n";
+	*/
+	
+//testing arrays
 	int A[4][4] = {
-		{6,5,3,4},
-		{1,2,4,5},
-		{5,4,2,3},
-		{1,2,4,5}
+	{6,5,3,4},
+	{1,2,4,5},
+	{5,4,2,3},
+	{1,2,4,5}
 	};
+
 	int B[4][4] = {
 		{1,2,4,5},
 		{5,4,2,3},
 		{1,3,4,2},
 		{6,5,3,4}
 	};
-	int output[outRow][outCol];
+
+
+	int output[outRow][outCol];		//specify output of matrix multiplication
 	 classicMult(A, B, output);
 	 print(output);
 
@@ -66,6 +85,4 @@ void print(int output[][outCol]) {
 		}
 		printf("\n");
 	}
-
-
 }
