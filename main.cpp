2f53154f851cf6e65c0015485341d4e256b0bf48
JimmyP8899 <80071092+JimmyP8899@users.noreply.github.com>
2022-11-01 22:48:13 -0700

    added timer kinda scuffed ngl

--- main.cpp
+++ main.cpp
@@ -3,61 +3,54 @@
 #include <cstdlib>
 #include <ctime>
 using namespace std;
-
-#define aRow 4		//Define A Row
-#define aCol 4		//Define A Column 
-#define bRow 4		//Define B Row
-#define bCol 4		//Define B Column
-#define outRow 4	//Define Output Row
-#define outCol 4	//Define Output Column
+#include <chrono>
+using namespace std::chrono;
+#define aRow 2		//Define A Row
+#define aCol 2		//Define A Column 
+#define bRow 2		//Define B Row
+#define bCol 2		//Define B Column
+#define outRow 2	//Define Output Row
+#define outCol 2    //Define Output Column
 
 
 void print(int output[][outCol]);
 void classicMult(int A[][aCol], int B[][bCol], int output[][outCol]);
 int main() {
-// MAIN METHOD TESTING METHOD
+	// MAIN METHOD TESTING METHOD
 
-//random number generator for arrays
-/*	srand(time(0));  // Initialize random number generator
-	int A[aRow][aCol];
-	for (int i = 0; i < aRow; i++) {
-		for (int j = 0; j < aCol; j++) {
-			A[i][j] = (rand() % 10);
+	//random number generator for arrays
+		srand(time(0));  // Initialize random number generator
+		int A[aRow][aCol];
+		for (int i = 0; i < aRow; i++) {
+			for (int j = 0; j < aCol; j++) {
+				A[i][j] = (rand() % 10);
+			}
 		}
-	}
-	int B[bRow][bCol];
-	for (int i = 0; i < bRow; i++) {
-		for (int j = 0; j < bCol; j++) {
-			B[i][j] = (rand() % 10);
+		int B[bRow][bCol];
+		for (int i = 0; i < bRow; i++) {
+			for (int j = 0; j < bCol; j++) {
+				B[i][j] = (rand() % 10);
+			}
 		}
-	}
-	cout << "Arrays successfully generated\n";
-	*/
-	
-//testing arrays
-	int A[4][4] = {
-	{6,5,3,4},
-	{1,2,4,5},
-	{5,4,2,3},
-	{1,2,4,5}
-	};
-
-	int B[4][4] = {
-		{1,2,4,5},
-		{5,4,2,3},
-		{1,3,4,2},
-		{6,5,3,4}
-	};
+		cout << "Arrays successfully generated\n";
 
 
+
+    auto start = high_resolution_clock::now();
 	int output[outRow][outCol];		//specify output of matrix multiplication
-	 classicMult(A, B, output);
-	 print(output);
+	classicMult(A, B, output);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Naive Multiplication Completed\n";
+    cout << "Runtime: " << duration.count() << " microseconds" << endl;
+ 
+
+
+//	print(output);
 
 
 
 
-	system("pause");
 	return 0;
 
 }
@@ -76,6 +69,11 @@ void classicMult(int A[][aCol], int B[][bCol], int output[][outCol])
 			}
 		}
 	}
+}
+
+void strassenMult(int A[][aCol], int B[][bCol], int output[][outCol]) {
+
+
 }
 
 void print(int output[][outCol]) {
