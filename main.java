ec27a3861912beeeeca710954f06f73479d43cca
JimmyP8899 <80071092+JimmyP8899@users.noreply.github.com>
2022-11-06 16:25:35 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,11 @@
 import java.lang.*;
 class Matrix {
-    public int size = 4;
+    public int size = 16;
     
     public static void main(String[] args) {
         Matrix m = new Matrix();
         
-        int size = 32;                                 ////////////////CHANGE SIZE
+        int size = 512;                                 ////////////////CHANGE SIZE
         int outputSize = size;                   
     
         
@@ -16,7 +16,7 @@ class Matrix {
         int [][] E = m.generateArray(size);   
         int [][] F = m.generateArray(size);      
    
-        int startTime, endTime, runTime1, runTime2, runTime3, runTime4, runTime5, average;
+        int startTime, endTime, runTime1, runTime2, runTime3, runTime4, runTime5;
         int [][] output = new int[outputSize][outputSize];
 
 
@@ -49,7 +49,7 @@ class Matrix {
         System.out.println("Runtimes: " + runTime1 + " " + runTime2 + " " + runTime3 + " " + runTime4 + " " + runTime5 + " Average: " + (double)(runTime1+runTime2+runTime3+runTime4+runTime5)/5);  
         
          ///////////////////////////////////////DIVIDEANDCONQUER////////////////////////////
-        
+      /*  
          startTime = (int) System.currentTimeMillis();
         System.out.println("Divide and Conquer Multiplication: ");
         startTime = (int) System.currentTimeMillis();
@@ -73,7 +73,33 @@ class Matrix {
         runTime5 = endTime - startTime;
         
         System.out.println("Runtimes: " + runTime1 + " " + runTime2 + " " + runTime3 + " " + runTime4 + " " + runTime5 + " Average: " + (double)(runTime1+runTime2+runTime3+runTime4+runTime5)/5);  
+        */
+
+        System.out.println("New Divide and Conquer Multiplication: ");
+        startTime = (int) System.currentTimeMillis();
+        output = m.divideandconquerStrassen(A, B, size);
+        endTime = (int) System.currentTimeMillis();
+        runTime1 = endTime - startTime;
+        startTime = (int) System.currentTimeMillis();
+        output = m.divideandconquerStrassen(C, D, size);
+        endTime = (int) System.currentTimeMillis();
+        runTime2 = endTime - startTime;
+        startTime = (int) System.currentTimeMillis();
+        output = m.divideandconquerStrassen(E, F, size);
+        endTime = (int) System.currentTimeMillis();
+        runTime3 = endTime - startTime;
+        startTime = (int) System.currentTimeMillis();
+        output = m.divideandconquerStrassen(A, C, size);
+        endTime = (int) System.currentTimeMillis();
+        runTime4 = endTime - startTime;
+        startTime = (int) System.currentTimeMillis();
+        output = m.divideandconquerStrassen(A, D, size);
+        endTime = (int) System.currentTimeMillis();
+        runTime5 = endTime - startTime;
         
+        System.out.println("Runtimes: " + runTime1 + " " + runTime2 + " " + runTime3 + " " + runTime4 + " " + runTime5 + " Average: " + (double)(runTime1+runTime2+runTime3+runTime4+runTime5)/5);  
+
+
         System.out.println("Strassen Multiplication: ");
         startTime = (int) System.currentTimeMillis();
         output = m.strassenMult(A, B, size);
@@ -98,6 +124,8 @@ class Matrix {
         
         System.out.println("Runtimes: " + runTime1 + " " + runTime2 + " " + runTime3 + " " + runTime4 + " " + runTime5 + " Average: " + (double)(runTime1+runTime2+runTime3+runTime4+runTime5)/5);  
 
+
+       
     }
     
     
@@ -274,4 +302,46 @@ private void combinedMatrix(int C[][], int A[][], int B[][], int rowC, int colC)
             }
         }
     }   
+    public int[][] divideandconquerStrassen(int[][]A, int[][]B, int size){
+        int[][] output = new int[size][size];
+        if(size == 1){
+            output[0][0] = A[0][0] * B[0][0];
+        }else{
+            int newSize = size/2;
+            
+            int[][] a = new int[newSize][newSize];
+            int[][] b = new int[newSize][newSize];
+            int[][] c = new int[newSize][newSize];
+            int[][] d = new int[newSize][newSize];
+            int[][] e = new int[newSize][newSize];
+            int[][] f = new int[newSize][newSize];
+            int[][] g = new int[newSize][newSize];
+            int[][] h = new int[newSize][newSize];
+  
+
+            split(A, a, 0, 0);
+            split(B, e, 0, 0);
+            split(A, b, 0, newSize);
+            split(B, f, 0, newSize);
+            split(A, c, newSize, 0);
+            split(B, g, newSize, 0);
+            split(A, d, newSize, newSize);
+            split(B, h, newSize, newSize);
+       
+            int[][] outputa = add(classicMult(a,e), classicMult(b,g));
+            int[][] outputb = add(classicMult(a,f), classicMult(b,h));
+            int[][] outputc = add(classicMult(c,e), classicMult(d,g));
+            int[][] outputd = add(classicMult(c,f), classicMult(d,h));
+
+            merge(outputa, output, 0, 0);
+            merge(outputb, output, 0, newSize);
+            merge(outputc, output, newSize, 0);
+            merge(outputd, output, newSize, newSize);
+            
+        }
+    
+        return output;
+    }
+
+
 }
